55bec8562b1abb3e40947da4f62757ecfa8c9d17
stzfao <79994072+stzfao@users.noreply.github.com>
2022-06-03 03:13:24 +0530

    removed sensitive info

--- client/src/firebase-config.js
+++ client/src/firebase-config.js
@@ -3,12 +3,7 @@ import { getAuth } from 'firebase/auth';
 
 // pexen's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAz58olUErDHJGpEyEhsuzOJzu6ojlPEto",
-  authDomain: "pexen-stzfao.firebaseapp.com",
-  projectId: "pexen-stzfao",
-  storageBucket: "pexen-stzfao.appspot.com",
-  messagingSenderId: "131311812346",
-  appId: "1:131311812346:web:693e39b895781e63801343"
+
 };
 
 // Initialize Firebase
